257018430e0d35ab21fee0c4b76e1fc31e85a221
César <csarfau@gmail.com>
2024-10-12 21:39:11 -0300

    merge main to feature---register---user

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,10 +8,10 @@ import { errorMiddleware } from './middlewares/errorMiddleware';
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(json());
 app.use('/api', Router);
-app.use(errorMiddleware);
+// app.use(errorMiddleware);
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT);
