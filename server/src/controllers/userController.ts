0a4e015009e981044642cd38852a622ed4aa1ba1
César <csarfau@gmail.com>
2024-10-13 21:59:15 -0300

    feature: register user route, login route  and auth middleware

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,16 +1,41 @@
 import { Request, Response } from 'express';
-import { IUserRepository } from '../types/user';
+import { IUser, IUserRepository } from '../types/user';
 import SuggestionGenerationService from '../services/suggestionGenerationService';
 import llm from '../openIA/chatOpenIA'
+import bcrypt, { compare } from 'bcrypt';
+import knex, { Knex } from 'knex';
+import { sign } from 'jsonwebtoken';
 
 export default class UserController {
 
     private readonly suggestionService: SuggestionGenerationService;
-
+    
     constructor(private readonly userRepository: IUserRepository, suggestionService?: SuggestionGenerationService) {
         this.suggestionService = suggestionService || new SuggestionGenerationService(llm);
     }
 
+    public async createUser(req: Request, res: Response) {
+        const { name, email, password } = req.body;
+
+        const user: IUser = {
+            name,
+            email,
+            password: await bcrypt.hash(password, 10)
+        }
+
+        return res.status(201).json({
+            data: await this.userRepository.createUser(user)
+        });
+    }
+
+    public async login(req: Request, res: Response) {
+        const { email, password } = req.body;
+        const credentials = { email, password };
+        const token = await this.userRepository.login(credentials);
+
+        return res.status(200).json({token});
+    } 
+    
     public async test(req: Request, res: Response) {
         return res.status(200).json({
             data: await this.userRepository.getUserByID(req.params.id)
