cb72bd915e9f636f613a76ee4de698f99ce284f4
Adrian Roger <adrianroge@gmail.com>
2024-10-16 17:55:26 -0300

    feat: create search controller and router

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import 'express-async-errors';
 import { json } from 'body-parser';
-import Router from './routes/routes';
+import router from './routes/routes';
 import { errorMiddleware } from './middlewares/errorMiddleware';
 
 dotenv.config();
@@ -11,8 +11,10 @@ const app = express();
 app.use(cors());
 
 app.use(json());
-app.use('/api', Router);
+app.use('/api', router);
 app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT);
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`)
+});
