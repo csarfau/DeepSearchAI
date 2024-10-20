3a4720d5e49dcc6664bf6e80b7b8427aa590f3d4
Adrian Roger <adrianroge@gmail.com>
2024-10-19 23:46:04 -0300

    fix: fixed wrong devlog string

--- server/src/middlewares/errorMiddleware.ts
+++ server/src/middlewares/errorMiddleware.ts
@@ -15,6 +15,6 @@ export const errorMiddleware = (
         });
     }
 
-    if(process.env.DEVMODE === 'true') console.log("DevMOde Error Loggin::\n", err);
+    if(process.env.DEVMODE === 'true') console.log("DevMOde Error Log::\n", err);
     return res.status(500).json({data: null, error: 'Internal server error'});
 }
