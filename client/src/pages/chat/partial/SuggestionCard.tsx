4e6ef5f01ce2292d9fec8cffff335e311178192e
unknown <junior_nakazono@hotmail.com>
2024-10-20 18:33:22 -0300

    Feat: Create response IA component, improve stepper component, connect prompt suggestion with trigger query

--- client/src/pages/chat/partial/SuggestionCard.tsx
+++ client/src/pages/chat/partial/SuggestionCard.tsx
@@ -3,58 +3,59 @@ import { iconsPath } from "../../components/Icons";
 import { theme } from "../../../App";
 
 export type SuggestionThemes = 
-        'trip' |
-        'cooking' |
-        'art' |
-        'technology' |
-        'programming' |
-        'politics' |
-        'sport' |
-        'history' |
-        'music';
+    'trip' |
+    'cooking' |
+    'art' |
+    'technology' |
+    'programming' |
+    'politics' |
+    'sport' |
+    'history' |
+    'music';
 
-export type IUserSuggestion = {
-    [key in SuggestionThemes]?: string;
-};
+export interface IUserSuggestion {
+    usersSuggestions: {
+        [key in SuggestionThemes]?: string
+    };
+    setterPromptChoice: React.Dispatch<React.SetStateAction<string>>;
+    setterTriggerResponse: React.Dispatch<React.SetStateAction<boolean>>
+}
 
-export const SuggestionCardSkeleton: React.FC<IUserSuggestion> = () => {
-  
+
+export const SuggestionCardSkeleton: React.FC = () => {
     return (
-        <Grid container spacing={2} sx={{justifyContent: 'center'}}>
+        <Grid container spacing={2} sx={{ justifyContent: 'center' }}>
             {Array.from({ length: 4 }).map((_, index) => (
                 <Grid size={{ xs: 12, sm: 6 }} key={index} sx={{ 
                     display: 'flex', 
                     alignItems: 'center', 
                     gap: '0.5rem',  
-                    maxWidth: {lg: '28rem'}, 
-                    p: '0.5rem'
+                    maxWidth: { lg: '28rem' }, 
+                    p: '0.5rem' 
                 }}>
-                    
-                     <Skeleton variant="circular" sx={{
+                    <Skeleton variant="circular" sx={{
                         width: '1.2rem', 
                         height: '1.2rem',
-                     }} />
-                     <Skeleton
+                    }} />
+                    <Skeleton
                         width={'100%'}
                         animation="wave"
                         height={24}
                     />
-
                 </Grid>
             ))}
         </Grid>
     );
 };
 
-export const SuggestionCard: React.FC<IUserSuggestion> = (suggestionTheme) => {
-    const iconTypes = Object.keys(suggestionTheme) as SuggestionThemes[];
-
+export const SuggestionCard: React.FC<IUserSuggestion> = ({ setterPromptChoice, setterTriggerResponse, usersSuggestions }) => {
+    const iconTypes = Object.keys(usersSuggestions) as SuggestionThemes[];
+    
     return (
-        <Grid container spacing={2} sx={{justifyContent: 'center'}}>
+        <Grid container spacing={2} sx={{ justifyContent: 'center' }}>
             {iconTypes.map((iconType) => (
-                
                 <Grid size={{ xs: 12, sm: 6 }} key={iconType} sx={{
-                    maxWidth:{lg: '28rem'},
+                    maxWidth: { lg: '28rem' },
                     cursor: 'pointer',
                     display: 'flex',
                     alignItems: 'center',
@@ -64,11 +65,18 @@ export const SuggestionCard: React.FC<IUserSuggestion> = (suggestionTheme) => {
                     borderRadius: '10rem',
                     boxShadow: '0px 1px 1px #818181',
                     color: theme.palette.primary.main
-                }}>
-                    <Box
+                }}
+                onClick={() => {
+                    console.log(usersSuggestions[iconType]);
+                    
+                    setterPromptChoice(usersSuggestions[iconType] || '');
+                    setterTriggerResponse(true);
+                }}
+                >
+                    <Box                       
                         sx={{
-                            width: '1.2rem', 
-                            height: '1.2rem', 
+                            width: '1.2rem',
+                            height: '1.2rem',
                             background: 'linear-gradient(147deg, rgba(151,0,209,1) 0%, rgba(157,0,200,1) 26%, rgba(222,0,105,1) 100%)',
                             WebkitMaskImage: `url(${iconsPath[iconType]})`,
                             WebkitMaskSize: 'cover',
@@ -76,7 +84,7 @@ export const SuggestionCard: React.FC<IUserSuggestion> = (suggestionTheme) => {
                             maskSize: 'cover',
                         }}
                     />
-                    <Typography variant="body1">{suggestionTheme[iconType]}</Typography>
+                    <Typography variant="body1">{usersSuggestions[iconType]}</Typography>
                 </Grid>
             ))}
         </Grid>
