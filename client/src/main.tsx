a58586d6f4e05d028650a13eab03c44c26db11cc
César <csarfau@gmail.com>
2024-10-21 21:53:26 -0300

    fix: create a dotenv in frontend

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,10 +4,11 @@ import App from './App.tsx'
 import './index.css'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 createRoot(document.getElementById('root')!).render(
   // <StrictMode>
-  <GoogleOAuthProvider clientId="671567557134-pskdc8dch3lc6keu2c8hkq05ttelld0s.apps.googleusercontent.com">
+  <GoogleOAuthProvider clientId={ clientId }>
     <App />
   </GoogleOAuthProvider>
   // </StrictMode>,
