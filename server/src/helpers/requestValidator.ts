c92ac7daeaf95b1a51ca0eac095a2f0e9e5018b3
Adrian Roger <adrianroge@gmail.com>
2024-10-23 17:31:00 -0300

    fix: update password validations

--- server/src/helpers/requestValidator.ts
+++ server/src/helpers/requestValidator.ts
@@ -1,7 +1,7 @@
 import { CustomError } from "./customError";
 import { IRequestBody, IRequestParams } from "../types/requestValidator";
 
-abstract class BaseRequestValidate<T>{
+abstract class BaseRequestValidate<T> {
   protected data: T;
 
   constructor(data: T) {
@@ -13,7 +13,7 @@ abstract class BaseRequestValidate<T>{
   }
 }
 
-export class RequestBodyValidator extends BaseRequestValidate<IRequestBody>{
+export class RequestBodyValidator extends BaseRequestValidate<IRequestBody> {
   private readonly emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
 
   public email(): this {
@@ -32,49 +32,46 @@ export class RequestBodyValidator extends BaseRequestValidate<IRequestBody>{
 
     if (!password) {
       throw new CustomError(400, "Password is required.");
-    } else {
-      if (password.length < 8 || password.length > 16) {
-        throw new CustomError(400, "Password must be between 8 and 16 characters long.");
-      }
-      if (!/[a-z]/.test(password)) {
-        throw new CustomError(400, 
-          "Password must contain at least one lowercase letter."
-        );
-      }
-      if (!/[A-Z]/.test(password)) {
-        throw new CustomError(400, 
-          "Password must contain at least one uppercase letter."
-        );
-      }
-      if (!/\d/.test(password)) {
-        throw new CustomError(400, "Password must contain at least one number.");
-      }
-      if (/[^A-Za-z0-9]/.test(password)) {
-        throw new CustomError(400, "Password must contain only letters and numbers.");
-      }
     }
+    if (password.length < 8 || password.length > 16) {
+      throw new CustomError(
+        400,
+        "Password must be between 8 and 16 characters long."
+      );
+    }
+    if (/[^A-Za-z0-9]/.test(password)) {
+      throw new CustomError(
+        400,
+        "Password must contain only letters and numbers."
+      );
+    }
+
     return this;
   }
 
-  public query():this{
-    const query: string = this.data.query || '';
+  public query(): this {
+    const query: string = this.data.query || "";
 
-    if(query.length < 5 || query.length > 2000){
-        throw new CustomError(400, "Query must be between 5 and 2000 characters long.");
+    if (query.length < 5 || query.length > 2000) {
+      throw new CustomError(
+        400,
+        "Query must be between 5 and 2000 characters long."
+      );
     }
-    return this
+    return this;
   }
 }
 
-export class RequestParamValidator extends BaseRequestValidate<IRequestParams>{
-  private readonly uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+export class RequestParamValidator extends BaseRequestValidate<IRequestParams> {
+  private readonly uuidRegex =
+    /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
-  public uuid():this{
-    const uuid:string = this.data.id || '';
-    if(!this.uuidRegex.test(uuid)){
+  public uuid(): this {
+    const uuid: string = this.data.id || "";
+    if (!this.uuidRegex.test(uuid)) {
       throw new CustomError(400, "Parameter ID is not a valid uuid v4.");
     }
 
-    return this
+    return this;
   }
 }
