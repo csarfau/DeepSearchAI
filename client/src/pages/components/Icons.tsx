9b272f92750c86e5659772a1388d76ce2f1f0df0
unknown <junior_nakazono@hotmail.com>
2024-10-25 01:30:34 -0300

    Enhancement: change icons path

--- client/src/pages/components/Icons.tsx
+++ client/src/pages/components/Icons.tsx
@@ -1,21 +1,21 @@
-import technology from '@/assets/icons/technology-icon.svg';
-import trip from '@/assets/icons/trip-icon.svg';
-import programming from '@/assets/icons/programming-icon.svg';
-import cooking from '@/assets/icons/cooking-icon.svg';
-import art from '@/assets/icons/art-icon.svg';
-import politics from '@/assets/icons/politic-icon.svg';
-import sport from '@/assets/icons/sport-icon.svg';
-import history from '@/assets/icons/history-icon.svg';
-import music from '@/assets/icons/music-icon.svg';
+import ConnectingAirportsIcon from '@mui/icons-material/ConnectingAirports';
+import Diversity2Icon from '@mui/icons-material/Diversity2';
+import ShareIcon from '@mui/icons-material/Share';
+import AssuredWorkloadIcon from '@mui/icons-material/AssuredWorkload';
+import LocalLibraryIcon from '@mui/icons-material/LocalLibrary';
+import HeadsetIcon from '@mui/icons-material/Headset';
+import SportsHandballIcon from '@mui/icons-material/SportsHandball';
+import PaletteIcon from '@mui/icons-material/Palette';
+import LocalDiningIcon from '@mui/icons-material/LocalDining';
 
 export const iconsPath = {
-    technology,
-    trip,
-    programming,
-    cooking,
-    art,
-    politics,
-    sport,
-    history,
-    music,
+    technology : <Diversity2Icon  sx={{color: '#572288f', width: '1rem'}}/>,
+    trip : <ConnectingAirportsIcon  sx={{color: '#572288f', width: '1rem'}}/>,
+    programming : <ShareIcon  sx={{color: '#572288f', width: '1rem'}}/>,
+    cooking: <LocalDiningIcon  sx={{color: '#572288f', width: '1rem'}}/>,
+    art: <PaletteIcon  sx={{color: '#572288f', width: '1rem'}}/>,
+    politics : <AssuredWorkloadIcon  sx={{color: '#572288f', width: '1rem'}}/>,
+    sport : <SportsHandballIcon  sx={{color: '#572288f', width: '1rem'}}/>,
+    history : <LocalLibraryIcon  sx={{color: '#572288f', width: '1rem'}}/>,
+    music: <HeadsetIcon sx={{color: '#572288f', width: '1rem'}}/>,
 } as const;
